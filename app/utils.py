90ad64f07f5f8e3b6d72e48ccd630695d52da935
Bradley Nichol <bradleynichol@live.co.uk>
2020-01-21 16:25:58 +0000

    planned output functionality added

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,10 @@
 import sqlite3
 from app import mail
 from flask_mail import Message
-from app.models import User
+from app.models import User, Schedule, ScheduleDetails
 from flask import url_for
+from datetime import datetime, date, timedelta
+from sqlalchemy import func
 
 
 # sqlite db connection
@@ -33,3 +35,38 @@ def send_reset_email(user):
 {url_for('users.reset_token', token=token, _external=True)}    
 '''
     mail.send(msg)
+
+
+
+def get_planned_output(line_num):
+
+    # get index of day
+    today_index = date.weekday(date.today())
+
+    # get week commencement date
+    wc_date = date.today() - timedelta(days=1)
+    
+    # get schedule id using the format date
+    sid = Schedule.query.filter(Schedule.wc_date==wc_date).first()
+
+    if sid:
+        # query for planned output based on day of the week
+        if today_index == 0:
+            planned_output = ScheduleDetails.query.with_entities(func.sum(ScheduleDetails.monday).label('sum')).filter_by(schedule_id=sid.id, line_num=line_num).first()
+        elif today_index == 1:
+            planned_output = ScheduleDetails.query.with_entities(func.sum(ScheduleDetails.tuesday).label('sum')).filter_by(schedule_id=sid.id, line_num=line_num).first()
+        elif today_index == 2:
+            planned_output = ScheduleDetails.query.with_entities(func.sum(ScheduleDetails.wednesday).label('sum')).filter_by(schedule_id=sid.id, line_num=line_num).first()
+        elif today_index == 3:
+            planned_output = ScheduleDetails.query.with_entities(func.sum(ScheduleDetails.thursday).label('sum')).filter_by(schedule_id=sid.id, line_num=line_num).first()
+        elif today_index == 4:
+            planned_output = ScheduleDetails.query.with_entities(func.sum(ScheduleDetails.friday).label('sum')).filter_by(schedule_id=sid.id, line_num=line_num).first()
+        elif today_index == 5:
+            planned_output = ScheduleDetails.query.with_entities(func.sum(ScheduleDetails.saturday).label('sum')).filter_by(schedule_id=sid.id, line_num=line_num).first()
+        elif today_index == 6:
+            planned_output = ScheduleDetails.query.with_entities(func.sum(ScheduleDetails.sunday).label('sum')).filter_by(schedule_id=sid.id, line_num=line_num).first()
+
+        return planned_output.sum
+    
+    else:
+        return 0
