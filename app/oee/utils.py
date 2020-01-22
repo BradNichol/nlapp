006f6866e201b165b7d740ce508a56d40c639147
Bradley Nichol <bradleynichol@live.co.uk>
2020-01-22 22:34:41 +0000

    moved block code from routes into dedicated functions

--- app/oee/utils.py
+++ app/oee/utils.py
@@ -1,6 +1,84 @@
 from app.models import User, Schedule, ScheduleDetails, OEEtbl
+from app.utils import db_connect
 from datetime import datetime, date, timedelta
 from sqlalchemy import func
+from flask import request, redirect
+
+
+def add_update_oee_details(oee_id, selectorType, selectTime, units):
+    """ Function to add / update OEE details and update job status """
+
+    # connect and update database
+    con = db_connect()
+    cur = con.cursor()
+
+    cur.execute("SELECT oee_id FROM OEE_details WHERE oee_id =:oee_id AND type=:type", {'oee_id':oee_id, 'type':selectorType})
+    row = cur.fetchall()
+
+    if len(row) != 1:
+
+        # Insert
+        sql = """INSERT INTO OEE_details (oee_id, type, {}) VALUES (?, ?, ?)""".format(selectTime)
+        cur.execute(sql, (oee_id, selectorType, units))
+        con.commit()
+
+    else:
+        # update
+        sql = """UPDATE OEE_details SET {}=? WHERE oee_id=? AND type=? """.format(selectTime)
+        cur.execute(sql, (units, oee_id, selectorType))
+        con.commit()
+        
+        # check if units produced > ordered units and update orders to 'Completed'
+        if selectorType == "Product":
+            update_job_status(oee_id)
+
+    con.close()
+            
+        
+    return 'Success'
+
+
+def update_job_status(oee_id):
+    """ Update job status to 'Complete' if produced qty > ordered qty """
+
+    # connect and update database
+    con = db_connect()
+    cur = con.cursor()
+
+    # with order_id I can sum all rows in table for orders that span across multiple days
+    cur.execute("""SELECT order_id, SUM(_07+_08+_09+_10+_11+_12+_13+_14+_15+_16+_17+_18+_19+_20+_21+_22) AS sum 
+                                        FROM OEE_details JOIN OEE ON OEE_details.oee_id = OEE.id 
+                                        WHERE type = 'Product' AND oee_id =:oee_id """, {'oee_id': oee_id})
+
+    row = cur.fetchall()
+    
+    order_id = row[0]['order_id']
+    totalUnits = row[0]['sum']
+
+
+    # get total ordered units
+    cur.execute("""SELECT units, status FROM orders WHERE order_id=:order_id""", {'order_id':order_id})
+    row = cur.fetchall()
+    try:
+        orderedUnits = row[0]['units']
+    except:
+        orderedUnits = 0
+
+    if totalUnits >= orderedUnits:
+    
+        # update 
+        sql = """UPDATE orders SET status=? WHERE order_id=? """
+        status = "Completed"
+        cur.execute(sql, (status, order_id))
+        con.commit()
+        con.close()
+
+        return 1
+    else:
+        return 0
+    
+    
+
 
 
 
