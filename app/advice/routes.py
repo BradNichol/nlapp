78468990103e4773a2fb759e8461170790ef2e9b
Bradley Nichol <bradleynichol@live.co.uk>
2020-01-22 21:02:31 +0000

    new advice text for schedule

--- app/advice/routes.py
+++ app/advice/routes.py
@@ -28,7 +28,7 @@ def adviceList(adviceTitle):
 
                 
         """
-        return text
+        
 
     if adviceTitle == 'OEESheetAdvice':
         text = """
@@ -52,8 +52,51 @@ def adviceList(adviceTitle):
         <strong>Planned Output</strong>
         <p>The planned output value has been set by Scheduling, and against previously
         agreed targets.</p>
-    
 
 
         """
-        return text
+
+    if adviceTitle == 'ScheduleOverviewAdvice':
+        text = """
+        <p>The weekly schedule is designed to give an overview of what products are to be produced each week,
+        and on what line they'll be produced. </p>
+        <p>This is also where you'll set the daily production target, that will inform the 
+        Conformance To Plan (CTP) score.</p>
+        
+        <strong>How To:</strong>
+        <ol>
+            <li>Click Add Schedule Button.</li>
+            <li>Select a week commencement date.</li>
+            <li>Save.</li>
+        </ol>
+
+        <p>Click on the newly created date to view the schedule.</p>
+
+        """
+    
+    if adviceTitle == 'ScheduleDetailAdvice':
+        text = """
+        <p>Here is where you'll set the production schedule for the chosen week.</p>
+
+        <strong>How To:</strong>
+        <ol>
+            <li>Click Add Product button.</li>
+            <li>Select a product and choose the line number for production.</li>
+            <li>Click Save.</li>
+            <li>Enter the planned output number for that product, each day.</li>
+            <li>Make sure to press update once any changes have been made.</li>
+        </ol>
+        <strong>Product Notes</strong>
+        <p>Once a product has been added for a particular production line, it cannot be added again.
+        It can only be added against an another line number. This is to prevent duplication issues.</p>
+
+        <strong>Notes on CTP</strong>
+        <p>For CTP to be added and tracked against each shift, a prior schedule
+        must have been setup and planned output entered.</p>
+        <p>If the OEE sheet for that shift has already begun, adding planned output numbers
+        afterwards, within the schedule, will have no affect on the score displayed on the 
+        OEE sheet or other reports.</p>
+
+
+        """
+    return text
