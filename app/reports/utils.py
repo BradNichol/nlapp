f710ee7a90a8479ddfc26f4a055e54ccf38d9347
BradNichol <bradleynichol@live.co.uk>
2020-03-08 17:20:49 +0000

    added SQL

--- app/reports/utils.py
+++ app/reports/utils.py
@@ -1,6 +1,41 @@
 
+from app.utils import db_connect
+
 
 # Function to convert SQLite result into an array
 # currently sums AM + PM shifts together
-def sql_to_arr(result):
-    return [i[0] + i[1] for i in result]
+def sql_to_arr(from_date, to_date, line_num, unit_type):
+    con = db_connect()
+    cur = con.cursor()
+
+    if unit_type == 'Product' or 'Rejects':
+        # get daily production count + no. day count 
+        cur.execute("""SELECT type, SUM(_07+_08+_09+_10+_11+_12+_13+_14) AS sum_am_count,
+                        SUM(_15+_16+_17+_18+_19+_20+_21+_22) AS sum_pm_count
+                        FROM OEE_details JOIN OEE ON OEE_details.oee_id = OEE.id 
+                        WHERE DATE(start_date) >= '{}' 
+                        AND DATE(start_date) <= '{}' AND line_num {} AND type='{}' GROUP BY start_date  """.format(from_date, to_date, line_num, unit_type ))
+        results = cur.fetchall()
+
+        arr = []
+        for row in results:        
+            if row[0] == unit_type:
+                arr.append(row[1] + row[2])
+        return arr
+
+def sql_to_arr2(from_date, to_date, line_num):
+    con = db_connect()
+    cur = con.cursor()
+    cur.execute("""SELECT SUM(_07+_08+_09+_10+_11+_12+_13+_14) AS sum_downtime_am,
+                    SUM(_15+_16+_17+_18+_19+_20+_21+_22) sum_downtime_pm 
+                    FROM OEE_details JOIN OEE ON OEE_details.oee_id = OEE.id 
+                    WHERE type != 'Product' AND type !='Rejects' 
+                    AND DATE(start_date) >= '{}' 
+                    AND DATE(start_date) <= '{}' AND line_num {} GROUP BY start_date """.format(from_date, to_date, line_num))
+    results = cur.fetchall()
+    
+    arr = []
+    for row in results:        
+        arr.append(row[0] + row[1])
+    return arr
+
