bba55930fcafceb3ac1f42d348f779e54e945cd4
BradNichol <bradleynichol@live.co.uk>
2020-03-08 11:46:18 +0000

    function added

--- app/reports/utils.py
+++ app/reports/utils.py
@@ -3,8 +3,7 @@
 # Function to convert SQLite result into an array
 # currently sums AM + PM shifts together
 def sql_to_arr(result):
-        arr = []
-        for i in result:
-            arr.append(i[0])
-        return arr
-    
+    arr = []
+    for i in result:
+        arr.append(i[0] + i[1])
+    return arr
