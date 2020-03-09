e9cfef28b904164efeefb9b29ad5724230241499
BradNichol <bradleynichol@live.co.uk>
2020-03-09 21:54:31 +0000

    oee_to_dict function created

--- app/reports/utils.py
+++ app/reports/utils.py
@@ -1,6 +1,6 @@
 
 from app.utils import db_connect
-
+from app.models import OEEcalc
 
 # Function to get product/reject numbers by date
 def get_product_count(from_date, to_date, line_num, unit_type):
@@ -32,3 +32,34 @@ def get_downtime_minutes(from_date, to_date, line_num, col_type):
     
     return results
 
+# function to pass a range of PAQ-OEE data into dictionary
+def oee_to_dict(downtime, line_speed, production, rejects):
+
+    """ total_reject in object variable set to 0. 
+        Manufacturing team are not currently recording rejects
+        but I've left this feature in. Can't use list currently until
+        data is present otherwise I get a out of index error. 
+        Change to rejects[i] once team start to use """ 
+   
+    
+    availability = []
+    performance = []
+    quality = []
+    oee_score = []
+
+    for i in range(len(production)):
+        data = OEEcalc(hourly_count=8, total_lost_minutes=downtime[i], CPM=line_speed[i], total_unit_count=production[i], total_rejects=0) # see notes on rejects above
+        availability.append(round((data.availability()*100),2))
+        quality.append(round((data.quality()*100),2))
+        performance.append(round((data.performance()*100),2))
+        oee_score.append(round((data.OEEscore()*100),2))
+
+     # add data into dictionary
+    OEE_dict = {
+        'availability' : availability,
+        'performance' : performance,
+        'quality' : quality,
+        'OEE' : oee_score    
+    }
+    
+    return OEE_dict
